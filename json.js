eb105e8f0a61c61d657e8f59af9016fe3f3b72a8
FelipeSmX <128720229+FelipeSmX@users.noreply.github.com>
2023-11-21 21:52:56 -0300

    Update json.js

--- json.js
+++ json.js
@@ -1,12 +1,16 @@
 {
   "pessoas": [
-    {
+    {  
+      "id":"1",
       "nome": "Felipe",
-      "idade": 20
+      "idade": 20,
+      "imagem":"https://photos.google.com/album/AF1QipNT4nLRxq8abgRGrDc-7MHaz4bIpzTxO3z_dJJ3/photo/AF1QipM8zOg8JHrcXUtQkVzWJ4hQglSMIE8z_HWE6lPF"
     },
     {
+      "id":"2",
       "nome": "Carlos",
-      "idade": 19
+      "idade": 19,
+      "imagem":"https://photos.google.com/album/AF1QipNT4nLRxq8abgRGrDc-7MHaz4bIpzTxO3z_dJJ3/photo/AF1QipM8zOg8JHrcXUtQkVzWJ4hQglSMIE8z_HWE6lPF"
     }
   ]
 }
